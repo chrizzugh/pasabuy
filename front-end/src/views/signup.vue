3911db08b27c3121e3d59c5eb21a6a3796e53160
Chrizza <55417444+chrzxx@users.noreply.github.com>
2021-05-01 17:12:38 +0800

    Add files via upload

--- front-end/src/views/signup.vue
+++ front-end/src/views/signup.vue
@@ -2,26 +2,16 @@
 
     <div class="flex items-center ">
         <router-link to="/">
-        <img src="/img/pasaBUYLogoOnly.png" class="w-20 h-16 block">
+        <img src="/img/pasaBUYLogoOnly.png" class="block w-20 h-16">
         </router-link>
-        <h1 class="absolute text-xl font-black tracking-widest  left-16 font-raleway
-          text-red-buttons block">pasaBUY</h1>
+        <h1 class="absolute block text-xl font-black tracking-widest left-16 font-raleway text-red-buttons">pasaBUY</h1>
       </div>
-    <div id="signup" class="flex items-center justify-center   pb-10  px-4">
-      <div class="w-full my-12 overflow-hidden text-center bg-white shadow-md flex-grow-1 rounded-xl
-      xl:w-2/5 lg:w-2/5 2xl:w-2/5 
-      md:w-97 sm:w-97
-      ">
+    <div id="signup" class="flex items-center justify-center px-4 pb-10">
+      <div class="w-full my-12 overflow-hidden text-center bg-white shadow-md flex-grow-1 rounded-xl xl:w-2/5 lg:w-2/5 2xl:w-2/5 md:w-97 sm:w-97 ">
         <div class="px-10 py-16 ">
             <h1 class="pb-5 space-x-1 space-y-1 text-2xl font-bold">Create an account</h1>
               <form action="#" class="space-y-3">
-               <div class="flex flex-col 
-                md:flex-row md:justify-between md:space-x-6
-                sm:flex-row sm:justify-between sm:space-x-6
-                xl:flex-row xl:justify-between xl:space-x-6
-                2xl:flex-row 2xl:justify-between 2xl:space-x-6
-                lg:flex-row lg:justify-between lg:space-x-6
-                 ">
+               <div class="flex flex-col md:flex-row md:justify-between md:space-x-6 sm:flex-row sm:justify-between sm:space-x-6 xl:flex-row xl:justify-between xl:space-x-6 2xl:flex-row 2xl:justify-between 2xl:space-x-6 lg:flex-row lg:justify-between lg:space-x-6 ">
                     <div  class="w-full">
                         <input name="" type="firstname"  required class="relative block w-full px-3 py-2 mt-4 mb-2 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="First Name" value="" />
                     </div>
@@ -32,19 +22,13 @@
                 <div class="mb-6">
                     <input aria-label="Email" name="" type="email" required class="relative block w-full px-3 py-2 mb-6 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Email" value="" />
                 </div>
-                <div class="mb-10 flex">
+                <div class="relative flex mb-10">
                     <div class="flex">
-                        <img class="h-11 w-10 absolute left-0" src="img/Philippines-Flag.svg ">
-                        <input aria-label="Phone Number" name="" type="text" required class="relative block w-full px-3 py-2 mb-6 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Phone Number" value="" />
+                        <img class="absolute w-8 h-12 rounded-md left-3 " src="img/Philippines-Flag.svg ">
+                        <input aria-label="Phone Number" name="" type="text" required class="block w-full px-3 py-2 pl-8 mb-6 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Phone Number" value="" />
                     </div>
                 </div>
-                <div class="flex flex-col  
-                md:flex-row md:justify-between md:space-x-6
-                sm:flex-row sm:justify-between sm:space-x-6
-                xl:flex-row xl:justify-between xl:space-x-6
-                2xl:flex-row 2xl:justify-between 2xl:space-x-6
-                lg:flex-row lg:justify-between lg:space-x-6
-                 ">
+                <div class="flex flex-col md:flex-row md:justify-between md:space-x-6 sm:flex-row sm:justify-between sm:space-x-6 xl:flex-row xl:justify-between xl:space-x-6 2xl:flex-row 2xl:justify-between 2xl:space-x-6 lg:flex-row lg:justify-between lg:space-x-6 ">
                     <div class="w-full" >
                         <input aria-label="Password" name="" type="password" required class="relative block w-full px-3 py-2 mb-6 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:z-10 focus:border-blue-300 " placeholder="Password" value="" />   
                     </div>
