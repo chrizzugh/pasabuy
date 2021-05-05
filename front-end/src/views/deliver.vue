c953275970424b6382715791b956897e4ddf44b0
lordsiinbal <79137266+lordsiinbal@users.noreply.github.com>
2021-05-05 09:35:35 +0800

    Update deliver.vue

--- front-end/src/views/deliver.vue
+++ front-end/src/views/deliver.vue
@@ -31,13 +31,12 @@ xsm:w-max
     lg:mr-2
     md:mr-2
     ">
-    <button @click=" isActive_function('btn1')" :class="{active: activeBtn === 0 }" type="button"
-    class="font-bold focus:outline-none text-red-buttons px-3 py-1 shadow  rounded-2xl bg-white w-full h-full"><a> All Deliveries</a></button>
+    <button @click=" isActive_function('btn1'),filter_obj('Deliveries')" :class="{active: activeBtn === 0 }" type="button"  class="font-bold focus:outline-none text-red-buttons px-3 py-1 shadow  rounded-2xl bg-white w-full h-full"><a> All Deliveries</a></button>
   </div>
   <div class="flex  items-center justify-between space-x-2">
-  <button  @click=" isActive_function('btn2')" :class="{active: activeBtn === 'btn2' }"  type="button"    class=" font-bold focus:outline-none text-green-150 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Complete</a></button>  
-  <button   @click=" isActive_function('btn3')" :class="{active: activeBtn === 'btn3' }"  type="button"   class=" font-bold focus:outline-none text-blue-700 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Confirmed</a></button>
-  <button  @click=" isActive_function('btn4')" :class="{active: activeBtn === 'btn4' }"  type="button"    class="font-bold focus:outline-none text-yellow-600 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Cancelled</a></button>
+  <button  @click=" isActive_function('btn2'),filter_obj('Completed')" :class="{active: activeBtn === 'btn2' }"  type="button"    class=" font-bold focus:outline-none text-green-150 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Complete</a></button>  
+  <button   @click=" isActive_function('btn3'),filter_obj('Confirmed')" :class="{active: activeBtn === 'btn3' }"  type="button"   class=" font-bold focus:outline-none text-blue-700 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Confirmed</a></button>
+  <button  @click=" isActive_function('btn4'),filter_obj('Cancelled')" :class="{active: activeBtn === 'btn4' }"  type="button"    class="font-bold focus:outline-none text-yellow-600 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Cancelled</a></button>
 </div>
 <div class="mt-3 
     2xl:ml-2
@@ -45,11 +44,11 @@ xsm:w-max
     lg:ml-2
     md:ml-2
     ">
-    <button @click=" isActive_function('btn5')" :class="{active: activeBtn === 'btn5' }" type="button"  class="font-bold focus:outline-none text-yellow-600 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a> In Transit</a></button>
+    <button @click=" isActive_function('btn5'),filter_obj('In Transit')" :class="{active: activeBtn === 'btn5' }" type="button"  class="font-bold focus:outline-none text-yellow-600 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a> In Transit</a></button>
   </div>
 </div>
 
-<div v-for="itemx in transaction" :key="itemx.id" >
+<div v-for="itemx in transaction_filter" :key="itemx.id" >
 
 <div class="p-5 w-full  flex flex-col space-y-4 items-center 
 2xl:p-0
@@ -177,6 +176,7 @@ lg:p-0
             <div class="flex flex-col p-3 space-y-4
             ">
             <span class="flex items-center space-x-2"><input type="radio" name="status" value="Completed"/><span>Completed</span></span>
+            <span class="flex items-center space-x-2"><input type="radio" name="status" value="Comfirmed"/><span>Confirmed</span></span>
             <span class="flex items-center space-x-2"><input type="radio" name="status" value="In Transit"/><span>In Transit</span></span>
             <span class="flex items-center space-x-2"><input type="radio" name="status" value="Cancelled"/><span>Cancelled</span></span>
            
@@ -215,6 +215,7 @@ export default {
   },
    created: function () {
     document.body.style.backgroundColor = "#EBEBEB";
+    this.transaction_filter=this.transaction
   },
   data(){
     return{
@@ -222,8 +223,9 @@ export default {
       toggle_status:false,
        activeBtn:0,
        trans_id:'',
+       transaction_filter:[],
        transaction:[
-        {
+          {
           id:1,
           street:'Rizal Street',
           city:'Legazpi City',
@@ -244,8 +246,50 @@ export default {
             }
           ]
         },
-        {
+       {
           id:2,
+          street:'Rizal Street',
+          city:'Legazpi City',
+          store:'SM City Legazpi',
+          payment:'Payment First',
+          profile_image:'https://i.pinimg.com/564x/80/e2/f6/80e2f60c9e41907b97300c337a40fa45.jpg',
+          name:'Yamete',
+          rate:'4.5',
+          order_number:'1234',
+          date:'February 23,2021',
+          time:'12:31 PM',   
+          currenTime:'',
+          currentDate:'',
+          selected: 'Confirmed',
+          items:[
+            {
+              list:'Pork',size:'1kl',brand:'Anybrand',quantity:2
+            }
+          ]
+        },
+        {
+          id:3,
+          street:'Rizal Street',
+          city:'Legazpi City',
+          store:'SM City Legazpi',
+          payment:'Payment First',
+          profile_image:'https://i.pinimg.com/564x/80/e2/f6/80e2f60c9e41907b97300c337a40fa45.jpg',
+          name:'Yamete',
+          rate:'4.5',
+          order_number:'1234',
+          date:'February 23,2021',
+          time:'12:31 PM',   
+          currenTime:'',
+          currentDate:'',
+          selected: 'Confirmed',
+          items:[
+            {
+              list:'Pork',size:'1kl',brand:'Anybrand',quantity:2
+            }
+          ]
+        },
+        {
+          id:4,
           street:'Rizal Street',
           city:'Legazpi City',
           store:'SM City Legazpi',
@@ -258,7 +302,7 @@ export default {
           time:'12:31 PM',   
           currenTime:'',
           currentDate:'',
-          selected: 'Confirmed',
+          selected: 'Completed',
           items:[
             {
               list:'Milk',size:'1kl',brand:'Anybrand',quantity:1
@@ -273,6 +317,14 @@ export default {
 }
 },
 methods:{
+    filter_obj(e){
+        if(e=='Deliveries'){
+         this.transaction_filter=[];
+         return this.transaction_filter=this.transaction
+        }
+        this.transaction_filter= this.transaction.filter(trans=>trans.selected==e)
+    
+    },
   update_stat(){
     let new_time=new Date();
     const index = this.transaction.findIndex(x=>x.id==this.trans_id);
@@ -289,6 +341,7 @@ methods:{
         this.  activeBtn= el;
         }
     },
+  
 }
 }
 
