d1b0d0fb79bde3da7410b8aaccf1d88d70ee2c43
ayra25 <79515466+ayra25@users.noreply.github.com>
2021-05-07 20:37:06 +0800

    Create cookiesPolicyMoreInformation.vue

---
+++ front-end/src/views/cookiesPolicyMoreInformation.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2 class="py-3 mt-10 text-2xl font-bold sm:text-xl vs:text-lg md:text-xl">More Information about Cookies</h2>
+
+  <p class="vs:text-xs">You can learn more about cookies: What Are Cookies?.</p><br>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
