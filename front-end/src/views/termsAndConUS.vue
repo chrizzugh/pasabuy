9b428b0efb79ce37039ee7a4a41e43edb243dfe2
ayra25 <79515466+ayra25@users.noreply.github.com>
2021-05-07 23:00:07 +0800

    Add files via upload

---
+++ front-end/src/views/termsAndConUS.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2 class="py-3 mt-10 text-2xl font-bold sm:text-xl vs:text-lg md:text-xl">United States Legal Compliance</h2>
+
+  <p class="vs:text-xs">You represent and warrant that (i) You are not located in a country that is subject to the United States government embargo, or that has been designated by the United States government as a "terrorist supporting" country, and (ii) You are not listed on any United States government list of prohibited or restricted parties.</p><br>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
