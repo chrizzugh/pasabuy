9bba98364570dd28fcfe2a9d38613d8a19d26c87
ayra25 <79515466+ayra25@users.noreply.github.com>
2021-05-10 19:42:09 +0800

    Add files via upload

---
+++ front-end/src/views/ContactUs.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2 class="py-3 mt-10 text-2xl font-bold sm:text-xl vs:text-lg md:text-xl">Contact Us </h2>
+
+  <p class="vs:text-xs">If you have any questions about this Cookies Policy, You can contact us:</p><br>
+        <ul class="pl-12 space-y-3 text-justify list-disc list-outside vs:text-xs">
+            <li>By email: kristoferbalang.alcantara@bicol-u.edu.ph</li>
+        </ul>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
