1d1d6eabf519cb3ea77d8d240dd76ee2843a65c1
lordsiinbal <79137266+lordsiinbal@users.noreply.github.com>
2021-05-22 06:33:15 +0800

    Add files via upload

--- front-end/src/views/helpCreateAcc.vue
+++ front-end/src/views/helpCreateAcc.vue
@@ -1,84 +1,80 @@
 <template>
-    <div class="pt-6 bg-gray- font-nunito">
-        <div id="helpCreateAcc" class="flex items-center justify-center w-full px-3 pb-16 mt-12 vs:pr-0">
-            <div class="w-full overflow-hidden font-bold divide-y divide-gray-400 rounded-xl divide-solid">
+    <div class=" pt-6 font-nunito overflow-y-auto">
+        <div id="helpCreateAcc" class="items-center flex justify-center w-full mt-9 pb-16 px-3 ">
+            <div class="overflow-hidden font-bold  w-full rounded-xl divide-y divide-gray-400 divide-solid">
                 <div class="pb-1">
                     <h1 class="space-x-1 space-y-1 text-xl">Creating an Account</h1>
                 </div> 
                 <div class="pt-2">
                 <!----sub section 1---->
-                    <div class="flex">
-                        <button @click="isOpen=!isOpen" class="w-full mb-2 text-sm font-bold text-left focus:outline-none">
-                        How do I create an account in Pasabuy?
+                    <div class="flex justify-between  items-center ">
+                        <h1 class="text-sm">How do I create an account in Pasabuy?</h1>
+                        <button @click="isOpen=!isOpen" class="font-bold focus:outline-none">
+                            <span class="material-icons text-2xl">arrow_drop_down</span>
                         </button>
-                        <button @click="isOpen=!isOpen" class="h-5 py-0 focus:outline-none">
-                        <span class="inline-flex float-right pl-12 align-top vs:pl-0 material-icons-round">arrow_drop_down</span> </button>
                     </div>
-                    <div class="w-full text-justify text-align-left ">
+                    <div class="w-full text-align-left justify">
                             <div v-if="isOpen">
-                                <ol class="pt-2 pb-3 text-sm font-normal leading-relaxed list-decimal list-outside vs:text-xs pl-9">
-                                    <li class="pl-0.5">Go to pasabuy.com and click Sign up.</li>
-                                    <li class="pl-0.5">Enter your name, email address, mobile phone number, and password.</li>
-                                    <li class="pl-0.5">Click Next.</li>
-                                    <li class="pl-0.5">Input the verification code sent to your email address and click Next.</li>
-                                    <li class="pl-0.5">Add your home address and click Next.</li>
-                                    <li class="pl-0.5">Upload a valid ID (you can temporarily skip this step you will be automatically logged in to your new account)</li>
-                                    <li class="pl-0.5">Wait for a few seconds for the verification of your  ID.</li>
-                                    <li class="pl-0.5">You will be prompted to the Log in page</li>
-                                    <li class="pl-0.5">Enter your registered email address and your password.</li>
+                                <ol class="list-decimal list-inside font-normal text-sm">
+                                    <li>Go to pasabuy.com and click Sign up.</li>
+                                    <li>Enter your name, email address, mobile phone number, and password.</li>
+                                    <li>Click Next.</li>
+                                    <li>Input the verification code sent to your email address and click Next.</li>
+                                    <li>Add your home address and click Next.</li>
+                                    <li>Upload a valid ID (you can temporarily skip this step you will be automatically logged in to your new account)</li>
+                                    <li>Wait for a few seconds for the verification of your  ID.</li>
+                                    <li>You will be prompted to the Log in page</li>
+                                    <li>Enter your registered email address and your password.</li>
                                 </ol>
                             </div>
                     </div>
                 <!----sub section 2---->
-                    <div class="flex">
-                        <button @click="isOpen1=!isOpen1" class="w-full mb-2 text-sm font-bold text-left focus:outline-none">
-                        How do I log in to my Pasabuy account?
+                    <div class="flex justify-between  items-center ">
+                        <h1 class="text-sm">How do I log in to my Pasabuy account?</h1>
+                        <button @click="isOpen1=!isOpen1" class="font-bold focus:outline-none">
+                            <span class="material-icons">arrow_drop_down</span>
                         </button>
-                        <button @click="isOpen1=!isOpen1" class="h-5 py-0 focus:outline-none">
-                        <span class="inline-flex float-right pl-12 align-top vs:pl-0 material-icons-round">arrow_drop_down</span> </button>
                     </div>
                     <div class="w-full text-align-left justify">
                             <div v-if="isOpen1">
-                                <ol class="pt-2 pb-3 text-sm font-normal leading-relaxed list-decimal list-outside pl-9">
-                                    <li class="pl-0.5">Go to pasabuy.com and click Log in.</li>
-                                    <li class="pl-0.5">Enter your registered email address and your password.</li>
-                                    <li class="pl-0.5">Make sure your password is correct</li>
+                                <ol class="list-decimal  list-inside font-normal text-sm">
+                                    <li>Go to pasabuy.com and click Log in.</li>
+                                    <li>Enter your registered email address and your password.</li>
+                                    <li>Make sure your password is correct</li>
                                 </ol>
                             </div>
                     </div>
                 <!----sub section 3---->
-                    <div class="flex">
-                        <button @click="isOpen2=!isOpen2" class="w-full mb-2 text-sm font-bold text-left focus:outline-none">
-                        How do I verify my Pasabuy account?
+                    <div class="flex justify-between  items-center ">
+                        <h1 class="text-sm">How do I verify my Pasabuy account?</h1>
+                        <button @click="isOpen2=!isOpen2" class="font-bold focus:outline-none">
+                            <span class="material-icons ">arrow_drop_down</span>
                         </button>
-                        <button @click="isOpen2=!isOpen2" class="h-5 py-0 focus:outline-none">
-                        <span class="inline-flex float-right pl-12 align-top vs:pl-0 material-icons-round">arrow_drop_down</span> </button>
                     </div>
                     <div class="w-full text-align-left justify">
                             <div v-if="isOpen2">
-                                <ol class="pt-2 pb-3 text-sm font-normal leading-relaxed list-decimal list-outside pl-9">
-                                    <li class="pl-0.5">Log in to your Pasabuy account.</li>
-                                    <li class="pl-0.5">Go to your profile.</li>
-                                    <li class="pl-0.5">Click Edit Profile.</li>
-                                    <li class="pl-0.5">Scroll down to the Verification Status and click Verify Now.</li>
-                                    <li class="pl-0.5">Upload the back and front photos of your ID.</li>
-                                    <li class="pl-0.5">Wait for a few seconds for verification of your ID</li>
+                                <ol class="list-decimal list-inside text-sm font-normal">
+                                    <li>Log in to your Pasabuy account.</li>
+                                    <li>Go to your profile.</li>
+                                    <li>Click Edit Profile.</li>
+                                    <li>Scroll down to the Verification Status and click Verify Now.</li>
+                                    <li>Upload the back and front photos of your ID.</li>
+                                    <li>Wait for a few seconds for verification of your ID</li>
                                 </ol>
                             </div>
                     </div>
                 <!---sub section 4----->
-                <div class="flex">
-                    <button @click="isOpen3=!isOpen3" class="w-full text-sm font-bold text-left focus:outline-none">
-                        I can’t create an account with my email address.
+                <div class="flex justify-between  items-center ">
+                        <h1 class="text-sm">I can’t create an account with my email address.</h1>
+                        <button @click="isOpen3=!isOpen3" class="font-bold focus:outline-none">
+                            <span class="material-icons ">arrow_drop_down</span>
                         </button>
-                        <button @click="isOpen3=!isOpen3" class="h-5 py-0 focus:outline-none">
-                        <span class="inline-flex float-right pl-12 align-top vs:pl-0 material-icons-round">arrow_drop_down</span> </button>
                     </div>
                     <div class="w-full text-align-left justify">
                             <div v-if="isOpen3">
-                                <ol class="pt-2 pb-3 text-sm font-normal leading-relaxed list-decimal list-outside pl-9">
-                                    <li class="pl-0.5">Keep in mind that an email address can only be associated with one (1) Pasabuy account.</li>
-                                    <li class="pl-0.5">If you've typed your email address correctly and see a message that an account with your email address already exists, let us know.</li>
+                                <ol class="list-decimal list-inside font-normal text-sm">
+                                    <li>Keep in mind that an email address can only be associated with one (1) Pasabuy account.</li>
+                                    <li>If you've typed your email address correctly and see a message that an account with your email address already exists, let us know.</li>
                                 </ol>
                             </div>
                     </div>
@@ -88,7 +84,10 @@
     </div>
 </template>
 
-<style>
+<style scoped>
+button{
+    outline:none;
+}
     #journal-scroll::-webkit-scrollbar {
         width: 5px;
         cursor: pointer;
--- front-end/src/views/helpExpandCon.vue
+++ front-end/src/views/helpExpandCon.vue
@@ -1,52 +1,49 @@
 <template>
-    <div class="pt-6 font-nunito"> <!--parent div-->
-        <div id="helpExpandConnection" class="flex items-center justify-center w-full px-3 pb-16 bg-transparent mt-9 ">
+    <div class=" pt-6 font-nunito"> <!--parent div-->
+        <div id="helpExpandConnection" class="items-center flex justify-center w-full mt-9  pb-16 bg-transparent px-3 ">
             
-            <div class="w-full overflow-hidden font-bold bg-transparent divide-y divide-gray-400 text-start rounded-xl divide-solid">
+            <div class="overflow-hidden font-bold text-start bg-transparent w-full rounded-xl divide-y divide-gray-400 divide-solid">
                 
                 <div class="pb-1">
-                    <h1 class="space-x-1 space-y-1 text-xl sm:text-base">Expanding your connections</h1>
+                    <h1 class="space-x-1 space-y-1 text-xl">Expanding your connections</h1>
                 </div> 
 
-                <div class="pt-2 text-justify">
+                <div class="pt-2">
                 <!---Sub section 1-->
-                    <div class="flex">
-                        <button @click="isOpen=!isOpen" class="w-full py-1 text-sm font-bold text-left focus:outline-none">
-                        How do I search for people to follow?
-                           </button>
-                           <button @click="isOpen=!isOpen" class="h-5 py-0 focus:outline-none">
-                        <span class="inline-flex float-right pl-12 align-top vs:pl-0 material-icons-round">arrow_drop_down</span> </button>
+                    <div class="flex justify-between items-center">
+                        <h1 class="text-sm">How do I search for people to follow?</h1>
+                        <button @click="isOpen=!isOpen" class="font-bold focus:outline-none">
+                            <span class="material-icons">arrow_drop_down</span>
+                        </button>
                     </div>
                     <div class="w-full text-align-left justify">
                             <div v-if="isOpen">
-                                <ol class="pt-2 pb-3 text-sm font-normal leading-relaxed list-decimal list-outside pl-9">
-                                    <li class="pl-0.5">Click on the Search tab on the upper right side of your screen.</li>
-                                    <li class="pl-0.5">Input keywords you want to search. It can be a name of a person or a place.</li>
-                                    <li class="pl-0.5">You can filter the search result by clicking the desired search result filter option above.</li>
-                                    <li class="pl-0.5">Click the Follow button beside the username that you want to follow.</li>
-                                    <li class="pl-0.5">You can also click on the photo of the user to visit their profile.</li>
+                                <ol class="list-decimal list-inside text-sm font-normal">
+                                    <li>Click on the Search tab on the upper right side of your screen.</li>
+                                    <li>Input keywords you want to search. It can be a name of a person or a place.</li>
+                                    <li>You can filter the search result by clicking the desired search result filter option above.</li>
+                                    <li>Click the Follow button beside the username that you want to follow.</li>
+                                    <li>You can also click on the photo of the user to visit their profile.</li>
                                 </ol>
                             </div>
                     </div>
                 <!----sub section 2---->
-                    <div class="flex">
-                        <button @click="isOpen1=!isOpen1" class="w-full py-1 text-sm font-bold text-left focus:outline-none">
-                            How do I give a rating to a person after having a transaction with them?
-                                                   
+                    <div class="flex justify-between items-center">
+                        <h1 class="text-sm">How do I give a rating to a person after having a transaction with them?</h1>
+                        <button @click="isOpen1=!isOpen1" class="font-bold focus:outline-none">
+                            <span class="material-icons">arrow_drop_down</span>
                         </button>
-                        <button @click="isOpen1=!isOpen1" class="h-5 py-0 focus:outline-none">
-                        <span class="inline-flex float-right pl-12 align-top vs:pl-0 material-icons-round">arrow_drop_down</span> </button>
                     </div>
                     <div class="w-full text-align-left justify">
                             <div v-if="isOpen1">
-                                <ol class="pt-2 pb-3 text-sm font-normal leading-relaxed list-decimal list-outside pl-9">
-                                    <li class="pl-0.5">Click your profile picture on the upper right corner of the screen.</li>
-                                    <li class="pl-0.5">Click Orders.</li>
-                                    <li class="pl-0.5">Search for the transaction you made with the user.</li>
-                                    <li class="pl-0.5">Click Write a review.</li>
-                                    <li class="pl-0.5">Select the number of stars you want to give to the user.</li>
-                                    <li class="pl-0.5">Write your feedback or comment on the space provided.</li>
-                                    <li class="pl-0.5">Click Publish Review if you’re done.</li>
+                                <ol class="list-decimal list-inside text-sm font-normal">
+                                    <li>Click your profile picture on the upper right corner of the screen.</li>
+                                    <li>Click Orders.</li>
+                                    <li>Search for the transaction you made with the user.</li>
+                                    <li>Click Write a review.</li>
+                                    <li>Select the number of stars you want to give to the user.</li>
+                                    <li>Write your feedback or comment on the space provided.</li>
+                                    <li>Click Publish Review if you’re done.</li>
                                 </ol>
                             </div>
                     </div>
