f7ec3a78ab0f37fa83583afb3b63c0574c93eb48
vinas-erica <79346463+vinas-erica@users.noreply.github.com>
2021-05-25 06:18:26 -0700

    Add files via upload

--- front-end/src/views/signup.vue
+++ front-end/src/views/signup.vue
@@ -10,54 +10,60 @@
       <div class="w-full my-12 overflow-hidden text-center bg-white shadow-md flex-grow-1 rounded-xl xl:w-2/5 lg:w-2/5 2xl:w-2/5 md:w-97 sm:w-97 ">
         <div class="px-10 py-16 ">
             <h1 class="pb-5 space-x-1 space-y-1 text-2xl font-bold">Create an account</h1>
-              <form action="#" class="space-y-3">
-               <div class="flex flex-col md:flex-row md:justify-between md:space-x-6 sm:flex-row sm:justify-between sm:space-x-6 xl:flex-row xl:justify-between xl:space-x-6 2xl:flex-row 2xl:justify-between 2xl:space-x-6 lg:flex-row lg:justify-between lg:space-x-6 ">
+              <form action="#">
+               <div class="relative flex md:justify-between md:space-x-6 sm:flex-row sm:justify-between sm:space-x-6 xl:flex-row xl:justify-between xl:space-x-6 2xl:flex-row 2xl:justify-between 2xl:space-x-6 lg:flex-row lg:justify-between lg:space-x-6 ">
                     <div class="w-full">
-                        <input name="" type="firstname"  required class="relative block w-full px-3 py-2 mt-4 mb-2 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="First Name" value="" />
+                        <input name="" type="firstname"  required class="relative block w-full px-3 py-2 mt-4 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="First Name" value="" />
                     </div>
                     <div  class="flex-row w-full">
-                        <input aria-label="Last Name" name="" type="name" required class="relative block w-full px-3 py-2 mt-4 mb-2 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Last Name" value="" />
-                        
+                        <input aria-label="Last Name" name="" type="name" required class="relative block w-full px-3 py-2 mt-4 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Last Name" value="" />
                     </div>
-                    <div class="absolute mt-5 inline-flex right-15.5">
-                        <div class=" arrowLeft inline-flex items-center justify-start p-4 bg-red-600 rounded-lg">
-                            <p class="text-base leading-none text-white">Are you sure you typed your name correctly?</p>
-                        </div>
-                    </div>
+                
                 </div>
-                <div class="flex flex-row">
-                    <input aria-label="Email" name="" type="email" required class="relative block w-full px-3 py-2 mb-2 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Email" value="" />
-                    <div class="absolute inline-flex right-32">
-                        <div class=" arrowLeft inline-flex items-center justify-start p-4 bg-red-600 rounded-lg">
-                            <p class="text-base leading-none text-white">Please enter a valid email address?</p>
-                        </div>
-                    </div>
+                <div v-show="true" class="mt-1 bottom-0 flex flex-row items-center">
+                    <span class="material-icons text-xs text-crimsonRed">
+                        warning</span> 
+                    <span><p class=" ml-1 text-xs leading-none text-crimsonRed font-semibold">Are you sure you typed your name correctly?</p></span>
                 </div>
-                <div class="relative flex mb-10 ">
-                    
+
+
+                <div class="flex flex-row mt-2">
+                    <input aria-label="Email" name="" type="email" required class="relative block w-full px-3 py-2 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Email" value="" />
+                </div>
+
+                <div v-show="true" class="mt-1 bottom-0 flex flex-row items-center">
+                    <span class="material-icons text-xs text-crimsonRed">
+                        warning</span> 
+                    <span><p class=" ml-1 text-xs leading-none text-crimsonRed font-semibold">Please enter a valid email</p></span>
+                </div>
+
+                <div class="relative flex mb-10 mt-2">
                         <span class="absolute z-20 bg-gray-200 top-2.5 left-4"><img class="w-9 h-9" src="img/philippines.png"></span>
-                        <input name="" type="text" class="w-full px-3 py-2 pl-16 mb-6 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Phone Number" value="" />
+                        <input name="" type="text" class="w-full px-3 py-2 pl-16 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Phone Number" value="" />
                     
                 </div>
                 <div class="flex flex-col md:flex-row md:justify-between md:space-x-6 sm:flex-row sm:justify-between sm:space-x-6 xl:flex-row xl:justify-between xl:space-x-6 2xl:flex-row 2xl:justify-between 2xl:space-x-6 lg:flex-row lg:justify-between lg:space-x-6 ">
                     <div class="w-full" >
-                        <input aria-label="Password" name="" type="password" required class="relative block w-full px-3 py-2 mb-6 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:z-10 focus:border-blue-300 " placeholder="Password" value="" />   
+                        <input aria-label="Password" name="" type="password" required class="relative block w-full px-3 py-2 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:z-10 focus:border-blue-300 " placeholder="Password" value="" />   
                     </div>
                     <div class="w-full" >
-                        <input aria-label="Confirm Password" name="" type="password" required class="relative block w-full px-3 py-2 mb-6 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Confirm Password" value="" />
+                        <input aria-label="Confirm Password" name="" type="password" required class="relative block w-full px-3 py-2 font-semibold tracking-wide text-gray-900 placeholder-gray-500 bg-gray-200 border rounded-lg appearance-none h-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Confirm Password" value="" />
                     </div>
-                    <div class="absolute inline-flex right-37">
-                        <div class=" arrowLeft inline-flex items-center justify-start p-4 bg-red-600 rounded-lg">
-                            <p class="text-base leading-none text-white">Those password did not match.</p>
-                        </div>
-                    </div>
+                    
                     <div class="absolute invisible inline-flex right-0"> <!--cant fit sa screen so tig set ko lng na invi for the mean time-->
                         <div class=" arrowLeft inline-flex items-center justify-start p-4 bg-red-600 rounded-lg">
                             <p class="text-base leading-none text-white">Use 8 or more characters.</p>
                         </div>
                     </div>
                 </div>
-                <div class="flex mb-2 -mx-1 ">
+
+                <div v-show="true" class="mt-1 bottom-0 flex flex-row items-center">
+                    <span class="material-icons text-xs text-crimsonRed">
+                        warning</span> 
+                    <span><p class=" ml-1 text-xs leading-none text-crimsonRed font-semibold">Those passwords did not match. Try again</p></span>
+                </div>
+
+                <div class="flex mb-2 -mx-1 mt-6">
                     <div class="w-1/2 px-1 mt-6 text-lg font-bold text-left text-grey-dark text-blue">
                         <router-link to="/log-in" >Log instead</router-link>
                     </div>
@@ -76,6 +82,11 @@
                         Privacy Policy
                     </router-link>
                 </div>
+
+                <!--<VueCustomTooltip label="This is a tooltip" position="is-bottom" class="rounded-full p-2 bg-gray-200">This is a tooltip
+                    <span>some text</span>
+                </VueCustomTooltip>-->
+            
             </form>
           </div>
         </div>
@@ -106,6 +117,7 @@
   font-size:24px;
   color:rgb(22, 22, 141);
 }
+
 .arrowLeft::before {
   content: '';
   position: absolute;
@@ -121,8 +133,12 @@
 </style>
 
 <script>
+//import VueCustomTooltip from '@adamdehaven/vue-custom-tooltip'
 export default {
-  
+  components: {
+      //VueCustomTooltip,
+    },
+
   created: function () {
     document.body.style.backgroundColor = "rgb(235,235,235)";
   },
