67b6ebaae9409ab0129435cfd15b26f1aa79f28e
llwyd-bot123 <61051378+llwyd-bot123@users.noreply.github.com>
2021-05-27 20:50:31 +0800

    Update uploadid.vue

--- front-end/src/views/uploadid.vue
+++ front-end/src/views/uploadid.vue
@@ -2,12 +2,14 @@
 
   <div class="flex items-center ">
       <router-link to="/">
-        <img src="/img/pasaBUYLogoOnly.png" class="w-16 h-16  block">
-      </router-link>
-        <h1 class="absolute text-xl font-black tracking-widest  left-16 font-raleway
-          text-red-buttons block
+        <button class="flex focus:outline-none">
+        <img src="/img/pasaBUYLogoOnly.png" class="w-16 h-16 ">
+        <p class="mt-4 text-xl font-black tracking-widest font-raleway
+          text-red-buttons 
           
-           ">pasaBUY</h1>
+           ">pasaBUY</p>
+        </button>
+        </router-link>
       </div>
 
     <div id="uploadedid" class="flex items-center w-full justify-center  px-4 pb-10 ">
