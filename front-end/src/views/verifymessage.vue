72a3fbbbe7882959995595a5e2affbe77f50e883
llwyd-bot123 <61051378+llwyd-bot123@users.noreply.github.com>
2021-05-27 20:49:51 +0800

    Update verifymessage.vue

--- front-end/src/views/verifymessage.vue
+++ front-end/src/views/verifymessage.vue
@@ -1,12 +1,14 @@
 <template class="font-nunito">
      <div class="flex items-center ">
        <router-link to="/">
-        <img src="/img/pasaBUYLogoOnly.png" class="w-16 h-16  block">
-        </router-link>
-        <h1 class="absolute text-xl font-black tracking-widest  left-16 font-raleway
-          text-red-buttons block
+        <button class="flex focus:outline-none">
+        <img src="/img/pasaBUYLogoOnly.png" class="w-16 h-16 ">
+        <p class="mt-4 text-xl font-black tracking-widest font-raleway
+          text-red-buttons 
           
-           ">pasaBUY</h1>
+           ">pasaBUY</p>
+        </button>
+        </router-link>
       </div>
     
     <div id="login" class="flex items-center justify-center w-full px-4  pb-10 ">
